dc3bddb6f17c81cbcabe121cf325302530e290d0
PedroMarcet04 <pedromarcet04@gmail.com>
2026-05-24 12:43:00 -0400

    Solve 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(
+        (obj) => !args.includes(obj)
+    );
 };
 
 // Do not edit below this line
