27eafbc3740e2f95d91f6bd325aa0111f79f172c
PedroMarcet04 <pedromarcet04@gmail.com>
2026-05-24 12:53:34 -0400

    Solve 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(low, high) {
+    if (
+        Number.isInteger(low) && low >= 0 &&
+        Number.isInteger(high) && high >= 0
+    ) {
+        if (low > high) {
+            const temp = low;
+            low = high;
+            high = temp;
+        }
+        let sum = 0;
+        for (let i = low; i <= high; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
