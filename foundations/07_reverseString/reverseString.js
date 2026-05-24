ba90e8fcfc1f840bc24fa0cb1e1e29e4c2a79f34
PedroMarcet04 <pedromarcet04@gmail.com>
2026-05-24 12:31:56 -0400

    Solve 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let ans = "";
+    for (const char of str) {
+        ans = char + ans
+    }
+    return ans
 };
 
 // Do not edit below this line
