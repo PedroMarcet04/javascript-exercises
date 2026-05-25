7a1971375223063307537eb8a9c8bcc1e6fcc697
PedroMarcet04 <pedromarcet04@gmail.com>
2026-05-25 13:05:24 -0400

    Solve 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replaceAll(/[^a-z0-9]/g,""); // Remove non-alphabetical/digit characters
+    let low = 0;
+    let high = str.length-1;
+    while (low < high) {
+        if (str.charAt(low)!=str.charAt(high)) {
+            return false;
+        }
+        low++;
+        high--;
+    }
+    return true;
 };
 
 // Do not edit below this line
