999151ae9d4bb2fab1aa032198cd54aa55904a72
PedroMarcet04 <pedromarcet04@gmail.com>
2026-05-25 14:47:32 -0400

    Solve 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let max_age = -1;
+    return people.reduce(
+        (oldestPerson, person) => {
+            const death = person.yearOfDeath==undefined ?
+                (new Date().getFullYear()) :
+                (person.yearOfDeath);
+            const age = death - person.yearOfBirth;
+            if (age > max_age) {
+                max_age = age;
+                return person;
+            } else {
+                return oldestPerson;
+            }},
+        undefined);
 };
 
 // Do not edit below this line
