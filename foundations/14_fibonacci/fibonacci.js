ae2ea7cd565ee483f33d0adba26e0b49e701864a
PedroMarcet04 <pedromarcet04@gmail.com>
2026-05-25 13:20:20 -0400

    Solve 14_fibonacci
    Used dynamic programming, which was overkill but an O(2^n) hurts my
    soul.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const dpFib = function(num, arr) {
+    if (arr.length > num) return arr[num];
+    dpFib(num-1, arr);
+    arr.push(arr[num-1]+arr[num-2]);
+    return arr[num];
+}
 
+const fibonacci = function(num) {
+    const arr = [0, 1, 1];
+    num = parseInt(num);
+    if (num == NaN || num < 0) return "OOPS";
+    return dpFib(num, arr);
 };
 
 // Do not edit below this line
